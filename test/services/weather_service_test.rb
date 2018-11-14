d16c0866208dbdb792c104e98cff13c0dd762760
Nardo <ronaldo.possan@gmail.com>
2018-11-14 13:19:29 +0000

    TDD for weather service object

--- test/services/weather_service_test.rb
+++ test/services/weather_service_test.rb
@@ -2,9 +2,22 @@ require 'test_helper'
 
 class WeatherServiceTest < Minitest::Test
 
+  def setup
+    @weather_service = WeatherService.new
+  end
+
   def test_new_service
-    ws = WeatherService.new
-    assert_instance_of WeatherService, ws
+    assert_instance_of WeatherService, @weather_service
+  end
+
+  def test_weather_by_city
+    w = @weather_service.find_by(city: "Campinas")
+    assert w.city == "Campinas"
+  end
+
+  def test_weather_by_latlon
+    w = @weather_service.find_by(latlon: {lat: 22, lon: 22})
+    assert w.city == "Campinas"
   end
 
 end
