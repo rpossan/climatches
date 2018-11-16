c755f02af88496be3e935c32d2bc9aa3794a8064
Nardo <ronaldo.possan@gmail.com>
2018-11-16 12:06:14 +0000

    Added a method to get average of degrees by day

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,4 +3,8 @@ class Weather < ApplicationRecord
   def celcius
     (temperature - 273.15).to_i
   end
+
+  def average_degrees(date=Date.today)
+    forecasts.where(date: Date.today).average(:degrees).to_i
+  end
 end
