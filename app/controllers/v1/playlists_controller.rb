e361d8270df4e53ec0fe7acd39e3a7667bdf51c8
Ronaldo Possan <ronaldo.possan@thomsonreuters.com>
2018-11-20 20:50:27 -0200

    Adds Heroku DB

--- app/controllers/v1/playlists_controller.rb
+++ app/controllers/v1/playlists_controller.rb
@@ -11,7 +11,7 @@ class V1::PlaylistsController < ApplicationController
     w_service = WeatherService.new
     weather = w_service.find_by(filter)
     pl_service = PlaylistService.new
-    playlist = pl_service.get_by_celsius(weather.celcius)
+    playlist = pl_service.get_by_celsius(weather.celcius, force_fetch)
     @playlist = { category: playlist.category, total: playlist.tracks.size }
     @playlist[:tracks] = playlist.tracks.map{|t| t.name }
     render json: @playlist, status: :ok
